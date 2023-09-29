f88d21d784092858e3eb4f490f9d1581c45f9f5e
Nintendoking01 <116310022+Nintendoking01@users.noreply.github.com>
2023-09-29 18:52:16 +0300

    Add files via upload

---
+++ courses/api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from api.models import CourseResource, CategoryResource
+from tastypie.api import Api
+
+api = Api(api_name='v1')
+api.register(CourseResource())
+api.register(CategoryResource())
+
+# api/v1/courses/     GET, POST
+# api/v1/courses/1/   GET, DELETE
+# api/v1/categories/  GET
+# api/v1/categories/1 GET
+
+
+urlpatterns = [
+    path('', include(api.urls), name='index')
+]
---
+++ courses/shop/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from .models import Course, Category
+# Create your views here.
+def index(request):
+    courses = Course.objects.all()
+    categories = Category.objects.all()
+    return render(request, 'shop/courses.html', {'courses': courses})
+
+def single_course(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, 'shop/single_course.html', {'course': course})
